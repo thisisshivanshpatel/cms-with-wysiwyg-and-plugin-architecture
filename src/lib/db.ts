708ecf52361a8d0f268cf228945dba68417ec10c
shivansh patel <shivanshpatel.1999@gmail.com>
2024-11-25 12:25:49 +0530

    Revert "updated the pool to directly connect with connection string"
    This reverts commit bc2573e742df10ec4e44874b26b06d00fcb78671.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,9 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // password: process.env.DB_PASSWORD,
-  // port: Number(process.env.DB_PORT),
-  // database: process.env.DB_NAME,
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
 });
