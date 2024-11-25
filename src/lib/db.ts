bc2573e742df10ec4e44874b26b06d00fcb78671
shivansh patel <shivanshpatel.1999@gmail.com>
2024-11-25 12:09:36 +0530

    updated the pool to directly connect with connection string

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,10 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // password: process.env.DB_PASSWORD,
+  // port: Number(process.env.DB_PORT),
+  // database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
 });
